bd8d5f049b6d0fac37895b04da82a023bb9fb902
Ronalis Saputra <macbookair@MacBook-Air-Macbook-2.local>
2023-03-08 02:13:34 +0800

    updated helmet data master

--- src/components/DataMaster.jsx
+++ src/components/DataMaster.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Layout from "./Layout";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const DataMaster = () => {
   const [users, setUsers] = useState([]);
@@ -15,28 +16,35 @@ const DataMaster = () => {
   console.log(users);
 
   return (
-    <Layout>
-      <div className="grid grid-cols-2 gap-2">
-        {users.map((user) => (
-          <Link
-            to={`/detail/${user.uuid}`}
-            key={user.uuid}
-            className="w-full rounded-md bg-gray-100 p-2 px-3 duration-150 ease-in-out hover:scale-105 hover:bg-gray-300"
-          >
-            <div className="flex items-center justify-between">
-              <div>
-                <p className="text-2xl font-light">{user.nama_ibu}</p>
-                <p className="text-2xl font-light">{user.nama_suami}</p>
+    <>
+      <Helmet>
+        <title>Data Master</title>
+      </Helmet>
+      <Layout>
+        <div className="grid grid-cols-2 gap-2">
+          {users.map((user) => (
+            <Link
+              to={`/detail/${user.uuid}`}
+              key={user.uuid}
+              className="w-full rounded-md bg-gray-100 p-2 px-3 duration-150 ease-in-out hover:scale-105 hover:bg-gray-300"
+            >
+              <div className="flex items-center justify-between">
+                <div>
+                  <p className="text-2xl font-light">{user.nama_ibu}</p>
+                  <p className="text-2xl font-light">{user.nama_suami}</p>
+                </div>
+                <div className="flex items-center space-x-5">
+                  <p className="text-2xl font-light">Jumlah anak</p>
+                  <p className="text-2xl font-semibold">
+                    {user.tb_bayis.length}
+                  </p>
+                </div>
               </div>
-              <div className="flex items-center space-x-5">
-                <p className="text-2xl font-light">Jumlah anak</p>
-                <p className="text-2xl font-semibold">{user.tb_bayis.length}</p>
-              </div>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </Layout>
+            </Link>
+          ))}
+        </div>
+      </Layout>
+    </>
   );
 };
 
